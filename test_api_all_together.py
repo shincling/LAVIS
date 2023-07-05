14f195d4f366436425e2e86a929d225c7d248f67
shincling <shijing609@163.com>
2023-07-05 09:09:16 +0000

    Modify the api for inference.

--- test_api_all_together.py
+++ test_api_all_together.py
@@ -9,18 +9,17 @@ from flask_api import FlaskAPI
 import requests
 import torch
 import numpy as np
-from lavis.models import load_model_and_preprocess
+from lavis.models import load_model_and_preprocess, load_preprocess_only
 import soundfile as sf
 import time
 import random
 from transformers import AutoProcessor
 import sys
-import torchaudio
 
 app = FlaskAPI(__name__)
 
 modal_type = sys.argv[1]
-assert modal_type in ['text', 'music', 'image', 'signal', 'speech', "image_med", "image_air", 'all'], "modal_type must be one of ['text', 'music', 'image', 'signal']"
+assert modal_type in ['text', 'music', 'image', 'signal', 'all'], "modal_type must be one of ['text', 'music', 'image', 'signal']"
 
 translation_api_cn2en_genel = 'http://172.18.30.134:6217/aliyuntranslate_gen/'
 translation_api_en2cn_genel = 'http://172.18.30.134:6220/aliyuntranslate_en_gen/'
@@ -76,6 +75,46 @@ def audio_file_reader(audio_path):
     audio_npy = audio_wav2vec_processor(audio_npy, sampling_rate=16000, return_tensors="pt",)
     
     return audio_npy
+
+def load_point_cloud(path:str):
+    """
+    从文件中读取点云
+    path: 点云路径,绝对路径
+    return: 点云, 字典类型, 包含 "coord", "color", "semantic_gt" 三个key
+    """
+    file_type = path.split(".")[-1]
+    if file_type == "pth":
+        cloud = torch.load(path)
+        if(isinstance(cloud, tuple)):
+            cloud = {"coord": cloud[0], "color": cloud[1], "semantic_gt": cloud[2]}
+            cloud["color"] = ((cloud["color"] + 1) * 127.5).astype(np.uint8)
+            cloud["color"] = cloud["color"].astype(np.float64)
+            cloud["coord"] = cloud["coord"].astype(np.float64)
+            # 把 coord 中的值归一化到 [-5, 5] 之间
+            max_value = np.max(cloud["coord"])
+            min_value = np.min(cloud["coord"])
+            final_value = max(abs(max_value), abs(min_value))
+            cloud["coord"] = cloud["coord"] / final_value  * 5.0
+
+        # "coord" "color" "semantic_gt"
+        if "semantic_gt" in cloud.keys():
+            cloud["semantic_gt"] = cloud["semantic_gt"].reshape([-1])
+            cloud["semantic_gt"] = cloud["semantic_gt"].astype(np.int64)
+    elif file_type == "ply":
+        cloud = {}
+        plydata = plyfile.PlyData().read(path)
+        points = np.array([list(x) for x in plydata.elements[0]])
+        coords = np.ascontiguousarray(points[:, :3]).astype(np.float64)
+        colors = np.ascontiguousarray(points[:, 3:6]).astype(np.float64)
+        semantic_gt = np.zeros((coords.shape[0]), dtype=np.int64)
+        cloud["coord"] = coords
+        cloud["color"] = colors
+        cloud["semantic_gt"] = semantic_gt
+    else:
+        raise ValueError("file type {} not supported".format(file_type))
+    
+    return cloud
+
 
 # setup device to use
 device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
@@ -133,29 +172,30 @@ if modal_type == 'music':
 if modal_type =="image":
     model, vis_processors, _ = load_model_and_preprocess(name="blip2_chatglm", model_type="pretrain_chatglm6b",\
                                                           is_eval=True, device=device, pre_model_path="/data2/shij/lavis_glm/lavis/checkpoints/checkpoint_4.pth")
-if modal_type =="image_med":
-    model, vis_processors, _ = load_model_and_preprocess(name="blip2_chatglm", model_type="pretrain_chatglm6b_image_dialog_ft",\
-                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_med_image_cap_ft_glm6b/20230613090/checkpoint_94.pth")
-                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_med_image_cap_ft_glm6b/20230613162/checkpoint_19.pth")
-                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_med_image_cap_ft_glm6b/20230614025/checkpoint_19.pth")
-                                                          is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_med_image_cap_ft_glm6b/20230614064/checkpoint_59.pth")
-if modal_type =="image_air":
-    model, vis_processors, _ = load_model_and_preprocess(name="blip2_chatglm", model_type="pretrain_chatglm6b_image_dialog_ft",\
-                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_med_image_cap_ft_glm6b/20230614064/checkpoint_59.pth")
-                                                          is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_aircraft_image_cap_ft_glm6b/20230615072/checkpoint_19.pth")
 if modal_type =="signal":
     model, vis_processors, _ = load_model_and_preprocess(name="blip2_chatglm", model_type="pretrain_chatglm6b-signal",\
+                                                          is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_signal_glm6b/20230518075/checkpoint_49.pth")
                                                         #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_signal_glm6b/20230518075/checkpoint_49.pth")
-                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_signal_glm6b/20230518075/checkpoint_49.pth")
-                                                          is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_signal_glm6b/20230519092/checkpoint_149.pth")
-if modal_type =="speech":
-    model, vis_processors, _ = load_model_and_preprocess(name="blip2_chatglm", model_type="pretrain_chatglm6b-speech",\
-                                                          is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/saved_checkpoints/speech_js_ft_0607.pth")
-                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/saved_checkpoints/speech_1000h_v0.pth")
+                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/output/BLIP2/Pretrain_stage2_signal_glm6b/20230519092/checkpoint_149.pth")
+if modal_type =="cloud":
+    model, vis_processors, _ = load_model_and_preprocess(
+        name="blip2_chatglm", model_type="pretrain_chatglm6b-cloud", \
+            pre_model_path="/data2/shij/data/cloud_cap/model/stage2/cap_and_structure_v3_chatglm.pth",is_eval=True, device=device
+    )
+
 if modal_type =="all":
+    all_model = "/data2/shij/codes/BLIP2/LAVIS/lavis/saved_checkpoints/merge_image_music_sig_cloud.pth"
+    # all_model = "/data2/shij/codes/BLIP2/LAVIS/lavis/saved_checkpoints/merge_image_ft_music_sig_cloud.pth"
     model, vis_processors, _ = load_model_and_preprocess(name="blip2_chatglm_all", model_type="pretrain_chatglm6b_dynamic",\
-                                                          is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/saved_checkpoints/merge_image_music_sig.pth")
-# del temp
+                                                        #   is_eval=True, device=device, pre_model_path="/data2/shij/codes/BLIP2/LAVIS/lavis/saved_checkpoints/merge_image_music_sig.pth")
+                                                          is_eval=True, device=device, pre_model_path=all_model)
+    print("load all-modal done.")
+    if "cloud" in all_model:
+        cloud_processor, _ = load_preprocess_only(name="blip2_chatglm", model_type="pretrain_chatglm6b-cloud", \
+            pre_model_path="/data2/shij/data/cloud_cap/model/stage2/cap_and_structure_v3_chatglm.pth",is_eval=True, device=device 
+        )
+        print("load cloud-modal processor done.")
+
 del _
 # prepare the image
 # image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
@@ -228,15 +268,14 @@ def image_infer():
         if isinstance(text, list):
             text = text[-1]
             assert isinstance(text, str), "text should be a string"
-        print("Revived text in model server-node: ", text)
 
         given_image = data.get('image', '')
         given_image = base64_pil(given_image).convert("RGB")
         given_image = vis_processors["eval"](given_image).unsqueeze(0).to(device)
 
         prompt = text
-        response = model.generate({"image": given_image, "prompt": prompt}, max_length=2048, repetition_penalty=2.0, temp_prompt="skip", early_stopping=False)
-        # response = model.generate({"image": given_image, "prompt": prompt}, max_length=320, repetition_penalty=2.5, temp_prompt="skip", early_stopping=True)
+        # response = model.generate({"image": given_image, "prompt": prompt}, max_length=300, repetition_penalty=2.0, temp_prompt="skip", early_stopping=False)
+        response = model.generate_image({"image": given_image, "prompt": prompt}, max_length=320, repetition_penalty=2.5, temp_prompt="skip", early_stopping=True)
         response = response[0]
 
         print("Response: ", response)
@@ -281,6 +320,7 @@ def text_infer():
             "response": response,
             "response_trans":response, 
             "input_text_en": 'Null',
+            "output_with_embeds": response
         }
         return json.dumps(result_dict, ensure_ascii=False, indent=4)
 
@@ -308,7 +348,7 @@ def audio_infer(use_prefix_prompt=False):
 
         prompt = text if not use_prefix_prompt else infer_prompt.format(text)
         # response = model.generate({"image": given_image, "prompt": prompt}, max_length=300, repetition_penalty=2.0, temp_prompt="skip", early_stopping=False)
-        response = model.generate_audio({"audio": given_audio, "prompt": prompt}, max_length=320, repetition_penalty=2.5, temp_prompt="skip", early_stopping=True)
+        response = model.generate_audio({"audio": given_audio, "prompt": prompt, "modality": "audio"}, max_length=320, repetition_penalty=2.5, temp_prompt="skip", early_stopping=True)
         response = response[0]
 
         print("Response: ", response)
@@ -317,7 +357,7 @@ def audio_infer(use_prefix_prompt=False):
             # response = response.split("</s>")[0]
         if response.endswith("</"):
             response = response[:-2]
-        print("Response: ", response)
+        # print("Response: ", response)
         # response = model.generate({"image": given_image, "prompt": prompt}, max_length=300, temp_prompt="skip")
         # response_trans = requests.post(translation_api_en2cn_genel, data={'text': response}).json()['Data']['Translated']
         response_trans = response.replace(" ", "")
@@ -332,7 +372,6 @@ def audio_infer(use_prefix_prompt=False):
         return json.dumps(result_dict, ensure_ascii=False, indent=4)
 
 @app.route("/music", methods=['GET', 'POST'])
-@app.route("/signal", methods=['GET', 'POST'])
 def music_infer(use_prefix_prompt=False):
     """
     List or create notes.
@@ -357,7 +396,7 @@ def music_infer(use_prefix_prompt=False):
         prompt = text if not use_prefix_prompt else infer_prompt.format(text)
         # response = model.generate({"image": given_image, "prompt": prompt}, max_length=300, repetition_penalty=2.0, temp_prompt="skip", early_stopping=False)
         print("Final prompt:", prompt)
-        response = model.generate_audio_or_music({"audio": given_audio, "prompt": prompt}, max_length=320, repetition_penalty=2.5, early_stopping=True)
+        response = model.generate_audio_or_music({"audio": given_audio, "prompt": prompt, "modality": "music"}, max_length=320, repetition_penalty=2.5, early_stopping=True)
         response = response[0]
 
         print("Response: ", response)
@@ -380,48 +419,31 @@ def music_infer(use_prefix_prompt=False):
         }
         return json.dumps(result_dict, ensure_ascii=False, indent=4)
 
-def get_ASRFeature(speech):
-    waveform, samplereate = torchaudio.load(speech)
-    print("waveform_size: ", waveform.size())
-    waveform_bytes = waveform.numpy().tobytes()
-    audio_base64 = base64.b64encode(waveform_bytes).decode('utf-8')
-
-    url = 'http://172.18.30.121:8888/genfeats'
-    asr_feature = requests.post(url, json={'waveform':audio_base64}).json()
-
-    asr_feature = base64.b64decode(asr_feature)
-    asr_feature = np.frombuffer(asr_feature, dtype=np.float32)
-    asr_feature = torch.tensor(asr_feature, dtype=torch.float32).view(-1, 512)
-    asr_feature = asr_feature.unsqueeze(0)
-    print(asr_feature.size())
-
-    return asr_feature
-
-@app.route("/speech", methods=['GET', 'POST'])
-def speech_infer(use_prefix_prompt=True):
+@app.route("/signal", methods=['GET', 'POST'])
+def signal_infer(use_prefix_prompt=False):
     """
     List or create notes.
     """
-    # infer_prompt = "指令： 给定上面的一段音乐，以下是一个描述任务的指令，请根据音乐相关的信息生成一个完成该指令的适当回复。\n\n 问题：{} \n\n 回答："
-    # infer_prompt = "请忠实地识别该语音[gMASK]"
-    infer_prompt = "以上是一段给定的语音，请识别给定的语音，直接生成合理的回复。"
     if request.method == 'POST':
         data = request.data
         text = data.get('text', '')
 
-        given_audio = data.get('speech', '')
+        given_audio = data.get('audio', '')
 
         formatted_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + str(random.randint(0, 1000000))
         audio_file_name = "/data2/shij/codes/BLIP2/temp_stuff/test_" + formatted_time + ".wav"
         write_wave(audio_file_name, bytes2wav(given_audio))
-        audio_cif_feats = get_ASRFeature(audio_file_name)
+        audio_file_npy = audio_file_reader(audio_file_name)
 
-        given_audio = audio_cif_feats
+        # mel branch 
+        # given_audio = vis_processors["eval"](audio_file_npy).unsqueeze(0).to(device)
 
-        prompt = text if not use_prefix_prompt else infer_prompt.format(text)
+        given_audio= audio_file_npy
+
+        prompt = text 
         # response = model.generate({"image": given_image, "prompt": prompt}, max_length=300, repetition_penalty=2.0, temp_prompt="skip", early_stopping=False)
         print("Final prompt:", prompt)
-        response = model.generate_speech({"speech_input": given_audio, "prompt": prompt})
+        response = model.generate_signal({"signal": given_audio, "prompt": prompt, "modality": "signal"}, max_length=320, repetition_penalty=2.5, early_stopping=True)
         response = response[0]
 
         print("Response: ", response)
@@ -435,6 +457,56 @@ def speech_infer(use_prefix_prompt=True):
         # response_trans = requests.post(translation_api_en2cn_genel, data={'text': response}).json()['Data']['Translated']
         response_trans = response.replace(" ", "")
 
+        result_dict = {
+            "input_text": text,
+            "prompt": text,
+            "response": response,
+            "response_trans": response_trans, 
+            "output_with_embeds": response_trans
+        }
+        return json.dumps(result_dict, ensure_ascii=False, indent=4)
+
+@app.route("/cloud", methods=['GET', 'POST'])
+def cloud_infer(use_prefix_prompt=False):
+    """
+    List or create notes.
+    """
+    if request.method == 'POST':
+        data = request.data
+        text = data.get('text', '')
+
+        given_cloud = data.get('cloud', '')
+        if given_cloud.endswith(".pth"):
+            cloud_path = "/data2/shij/data/cloud_cap/example_data/" + given_cloud
+            assert os.path.exists(cloud_path), "Cloud file not found!"
+        else:
+            formatted_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + str(random.randint(0, 1000000))
+            cloud_file_name = "/data2/shij/codes/BLIP2/temp_stuff/test_" + formatted_time + ".pth"
+            clund_data = base64.b64decode(given_cloud)
+            with open(cloud_file_name, 'wb') as f:
+                f.write(clund_data)
+            cloud_path = cloud_file_name
+
+        prompt = text 
+        # response = model.generate({"image": given_image, "prompt": prompt}, max_length=300, repetition_penalty=2.0, temp_prompt="skip", early_stopping=False)
+        print("Final prompt:", prompt)
+
+        # cloud = torch.load(cloud_path)
+        cloud = load_point_cloud(cloud_path)
+        cloud = cloud_processor["eval"](cloud)
+        for k in cloud.keys():
+            if(isinstance(cloud[k], torch.Tensor)):
+                cloud[k] = cloud[k].to(device)
+                cloud[k] = cloud[k].unsqueeze(0)
+
+        cloud_copy = cloud.copy()
+        cloud = cloud_copy.copy()
+        # result = model.generate_with_hidden_prompt({"cloud":cloud, "text_input": "请描述一下这个三维场景。"}, max_length=100, num_beams=1)
+        response = model.generate_cloud({"cloud":cloud, "text_input": prompt}, max_length=100, num_beams=1)
+        print("cloud response:", response)
+        if isinstance(response, list):
+            response = response[0]
+        response_trans = response.replace(" ", "")
         result_dict = {
             "input_text": text,
             "prompt": text,
@@ -447,20 +519,15 @@ def speech_infer(use_prefix_prompt=True):
 if __name__ == "__main__":
     # app.config[‘JSON_AS_ASCII’] = False 这个跟编码有关系，好像可以直接弄
     # app.run(debug=False, host='0.0.0.0', port=7124)
-    if modal_type == "image_med":
-        app.run(debug=False, host='0.0.0.0', port=7122)
-    if modal_type == "image_air":
-        app.run(debug=False, host='0.0.0.0', port=7121)
     if modal_type == "image":
         app.run(debug=False, host='0.0.0.0', port=7125)
     if modal_type == "music":
         app.run(debug=False, host='0.0.0.0', port=7126)
-    if modal_type == "speech":
-        app.run(debug=False, host='0.0.0.0', port=7126)
     if modal_type == "signal":
         app.run(debug=False, host='0.0.0.0', port=7127)
     if modal_type == "all":
-        app.run(debug=False, host='0.0.0.0', port=7128)
+        # app.run(debug=False, host='0.0.0.0', port=7128)
+        app.run(debug=False, host='0.0.0.0', port=7129)
 
 """
 model.generate({
